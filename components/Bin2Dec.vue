659bebd1a77d82ad8147a3f5d32046ac593e4d07
Matej <matejchalachan@gmail.com>
2020-07-01 18:42:33 +0200

    finished bin2dec

--- components/Bin2Dec.vue
+++ components/Bin2Dec.vue
@@ -6,8 +6,28 @@
       state="dark"
       color="dark"
       shadow
-      @keyup="convertBinary"
+      @keyup="checkBinary"
     />
+
+    <vs-alert
+      v-show="errorMessage"
+      title="Danger"
+      color="danger"
+      style="margin-bottom: 15px;
+    margin-top: 15px;"
+    >
+      {{ errorMessage }}
+    </vs-alert>
+
+    <div v-show="!errorMessage" class="btn-wrapper">
+      <vs-button color="dark" type="filled" size="large" @click="convertBinary">
+        Show Decimal
+      </vs-button>
+    </div>
+
+    <div class="output">
+      {{ decimal }}
+    </div>
   </div>
 </template>
 
@@ -16,17 +36,42 @@ export default {
   name: 'Bin2Dec',
   data () {
     return {
-      binaryNumber: ''
+      binaryNumber: '',
+      decimal: '',
+      errorMessage: ''
     }
   },
   methods: {
+    checkBinary () {
+      for (const char of this.binaryNumber) {
+        if (char !== '0' && char !== '1') {
+          this.errorMessage = 'Binary number can only contain 0 a 1'
+        } else {
+          this.errorMessage = ''
+        }
+      }
+    },
     convertBinary () {
+      let decimal = 0
+      for (let i = this.binaryNumber.length - 1; i >= 0; i--) {
+        decimal += parseInt(this.binaryNumber[i]) * Math.pow(2, this.binaryNumber.length - 1 - i)
+      }
 
+      this.decimal = decimal
     }
   }
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+.btn-wrapper {
+  padding-bottom: 15px;
+  padding-top: 15px;
+  display: flex;
+  justify-content: center;
+}
 
+.output {
+  padding: 15px;
+}
 </style>
