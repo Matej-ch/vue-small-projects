1283fd52b3a549c7b0c4ee2d84525c8511934c8f
Matej <matejchalachan@gmail.com>
2020-07-09 22:39:47 +0200

    neumorphic design for navigation

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,41 +1,41 @@
 <template>
-  <nav>
-    <div>
+  <nav class="navigation">
+    <div class="link">
       <nuxt-link :to="{name: 'index'}">
         Home
       </nuxt-link>
     </div>
-    <div>
+    <div class="link">
       <nuxt-link :to="{name: 'border'}">
         Border radius previewer
       </nuxt-link>
     </div>
 
-    <div>
+    <div class="link">
       <nuxt-link :to="{name: 'bindec'}">
         Bin 2 Dec
       </nuxt-link>
     </div>
 
-    <div>
+    <div class="link">
       <nuxt-link :to="{name: 'freq'}">
         Word frequency
       </nuxt-link>
     </div>
 
-    <div>
+    <div class="link">
       <nuxt-link :to="{name: 'truefalse'}">
         True or False
       </nuxt-link>
     </div>
 
-    <div>
+    <div class="link">
       <nuxt-link :to="{name: 'github'}">
         Github status
       </nuxt-link>
     </div>
 
-    <div>
+    <div class="link">
       <nuxt-link :to="{name: 'calc'}">
         Calculator
       </nuxt-link>
