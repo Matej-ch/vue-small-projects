e93a5a7c3888243f6b61bc9710f207a1096efef8
Matej <matejchalachan@gmail.com>
2022-07-26 18:39:01 +0200

    bin2dec style updated

--- components/Bin2Dec.vue
+++ components/Bin2Dec.vue
@@ -1,21 +1,45 @@
 <template>
-    <div class="container">
-        <label for="">Binary number
-            <input v-model="binaryNumber" @keyup="checkBinary">
-        </label>
 
-        <div v-show="errorMessage" title="Danger" class="bg-red-500" style="margin-bottom: 15px;margin-top: 15px;">
-            {{errorMessage}}
-        </div>
+    <div class="min-w-screen bg-gray-100 flex flex-col items-center justify-center">
+        <div class="rounded-xl bg-gradient-to-b from-blue-600 to-blue-400 mr-3">
+            <div class="flex flex-col">
+                <div class="flex flex-col items-center justify-center text-white py-4 bg-blue-800">
+                    <div class="text-center uppercase text-2xl">Binary to deca convertor</div>
+                </div>
 
-        <div v-show="!errorMessage" class="btn-wrapper">
-            <button @click="convertBinary">
-                Show Decimal
-            </button>
-        </div>
+                <div class="px-4 py-5">
+                    <div class="flex flex-col text-white">
 
-        <div class="output">
-            {{decimal}}
+                        <div class="flex items-end justify-between mb-5">
+                            <div class="flex flex-col text-center w-3/6 px-2">
+                                <label class="mb-1" for="weight-kilograms">Binary number</label>
+
+                                <input v-model="binaryNumber" @keyup="checkBinary"
+                                       class="py-3 px-5 rounded focus:outline-none text-gray-600 focus:text-gray-600"
+                                >
+                            </div>
+                            <div class="flex flex-col text-center w-3/6 px-2">
+                                <button v-show="!errorMessage" @click="convertBinary"
+                                        class="bg-green-600 text-white px-4 py-2 rounded-md text-1xl font-medium hover:bg-green-700 transition duration-300">
+                                    Show Decimal
+                                </button>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div class="flex items-center justify-between mb-3 w-full flex-col">
+                        <div class="flex flex-col text-right px-2 font-bold text-xl">
+                            {{decimal}}
+                        </div>
+                        <div v-show="errorMessage" class="bg-red-500 flex flex-col text-center px-2 font-bold"
+                             style="margin-bottom: 15px;margin-top: 15px;">
+                            {{errorMessage}}
+                        </div>
+                    </div>
+
+                </div>
+            </div>
         </div>
     </div>
 </template>
