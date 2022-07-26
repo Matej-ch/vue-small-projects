29b85b497af44253cc07708f76390ab1072b9248
Matej <matejchalachan@gmail.com>
2022-07-26 18:25:13 +0200

    navbar and store fixed

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,19 +2,18 @@ import {defineNuxtConfig} from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    ssr: false,
     alias: {
         "@app": "/@app"
     },
     css: ["@/assets/main.scss"],
-    modules: ['@nuxtjs/tailwindcss'],
     postcss: {
         plugins: {
             tailwindcss: {},
             autoprefixer: {},
         },
     },
-    buildModules: [
+    modules: [
+        ['@nuxtjs/tailwindcss'],
         [
             '@pinia/nuxt',
             {
---
+++ store/navbarStore.js
@@ -0,0 +1,17 @@
+import {defineStore} from 'pinia'
+
+export const useNavbarStore = defineStore('navbar', {
+    state: () => {
+        return {
+            items: [
+                {url: 'index', label: 'Home'},
+                {url: 'border', label: 'Border radius previewer'},
+                {url: 'bindec', label: 'Bin 2 Dec'},
+                {url: 'freq', label: 'Word frequency'},
+                {url: 'truefalse', label: 'True or False'},
+                {url: 'github', label: 'Github status'},
+                {url: 'calc', label: 'Calculator'},
+            ]
+        };
+    }
+})
