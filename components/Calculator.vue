5879dbc588a4887ce6e9c4e9f1831fc0635365c4
Matej <matejchalachan@gmail.com>
2022-07-28 14:48:47 +0200

    updated calculator

--- components/Calculator.vue
+++ components/Calculator.vue
@@ -2,8 +2,9 @@
 
     <div class="w-full mx-auto rounded-xl bg-gray-100 shadow-xl text-gray-800 relative overflow-hidden"
          style="max-width:300px">
-        <div class="w-full h-40 bg-gradient-to-b from-gray-800 to-gray-700 flex items-end text-right">
-            <div class="w-full py-5 px-6 text-6xl text-white font-thin">{{result}}</div>
+        <div class="w-full h-40 bg-gradient-to-b from-gray-800 to-gray-700 flex items-end text-right flex-col">
+            <div class="w-full pt-4 px-6 text-3xl text-white font-thin">{{operation}}</div>
+            <div class="w-full py-5 px-6 text-6xl text-white font-thin mt-auto">{{result}}</div>
         </div>
 
         <div class="w-full bg-gradient-to-b from-indigo-400 to-indigo-500">
@@ -22,13 +23,13 @@
                     </button>
                 </div>
                 <div class="w-1/4 border-r border-b border-indigo-400">
-                    <button @click="operation = '%'"
+                    <button @click="appendOperation('%')"
                             class="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-opacity-50 text-xl font-light">
                         %
                     </button>
                 </div>
                 <div class="w-1/4 border-r border-b border-indigo-400">
-                    <button @click="operation = '/'"
+                    <button @click="appendOperation('/')"
                             class="w-full h-16 outline-none focus:outline-none bg-indigo-700 bg-opacity-10 hover:bg-opacity-20 text-white text-2xl font-light">
                         ÷
                     </button>
@@ -37,25 +38,25 @@
 
             <div class="flex w-full">
                 <div class="w-1/4 border-r border-b border-indigo-400">
-                    <button
-                        class="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-xl font-light">
+                    <button @click="appendNumber(7)"
+                            class="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-xl font-light">
                         7
                     </button>
                 </div>
                 <div class="w-1/4 border-r border-b border-indigo-400">
-                    <button
-                        class="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-xl font-light">
+                    <button @click="appendNumber(8)"
+                            class="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-xl font-light">
                         8
                     </button>
                 </div>
                 <div class="w-1/4 border-r border-b border-indigo-400">
-                    <button
-                        class="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-xl font-light">
+                    <button @click="appendNumber(9)"
+                            class="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-xl font-light">
                         9
                     </button>
                 </div>
                 <div class="w-1/4 border-r border-b border-indigo-400">
-                    <button @click="operation = '*'"
+                    <button @click="appendOperation('*')"
                             class="w-full h-16 outline-none focus:outline-none bg-indigo-700 bg-opacity-10 hover:bg-opacity-20 text-white text-xl font-light">
                         ⨉
                     </button>
@@ -64,25 +65,25 @@
 
             <div class="flex w-full">
                 <div class="w-1/4 border-r border-b border-indigo-400">
-                    <button
-                        class="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-xl font-light">
+                    <button @click="appendNumber(4)"
+                            class="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-xl font-light">
                         4
                     </button>
                 </div>
                 <div class="w-1/4 border-r border-b border-indigo-400">
-                    <button
-                        class="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-xl font-light">
+                    <button @click="appendNumber(5)"
+                            class="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-xl font-light">
                         5
                     </button>
                 </div>
                 <div class="w-1/4 border-r border-b border-indigo-400">
-                    <button
-                        class="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-xl font-light">
+                    <button @click="appendNumber(6)"
+                            class="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-xl font-light">
                         6
                     </button>
                 </div>
                 <div class="w-1/4 border-r border-b border-indigo-400">
-                    <button @click="operation = '-'"
+                    <button @click="appendOperation('-')"
                             class="w-full h-16 outline-none focus:outline-none bg-indigo-700 bg-opacity-10 hover:bg-opacity-20 text-white text-xl font-light">
                         -
                     </button>
@@ -90,26 +91,26 @@
             </div>
             <div class="flex w-full">
                 <div class="w-1/4 border-r border-b border-indigo-400">
-                    <button
-                        class="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-xl font-light">
+                    <button @click="appendNumber(1)"
+                            class="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-xl font-light">
                         1
                     </button>
                 </div>
                 <div class="w-1/4 border-r border-b border-indigo-400">
-                    <button
-                        class="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-xl font-light">
+                    <button @click="appendNumber(2)"
+                            class="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-xl font-light">
                         2
                     </button>
                 </div>
                 <div class="w-1/4 border-r border-b border-indigo-400">
-                    <button
-                        class="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-xl font-light">
+                    <button @click="appendNumber(3)"
+                            class="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-xl font-light">
                         3
                     </button>
                 </div>
                 <div class="w-1/4 border-r border-b border-indigo-400">
                     <button
-                        @click="operation = '+'"
+                        @click="appendOperation('+')"
                         class="w-full h-16 outline-none focus:outline-none bg-indigo-700 bg-opacity-10 hover:bg-opacity-20 text-white text-xl font-light">
                         +
                     </button>
@@ -117,20 +118,20 @@
             </div>
             <div class="flex w-full">
                 <div class="w-1/4 border-r border-indigo-400">
-                    <button
-                        class="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-xl font-light">
+                    <button @click="appendNumber(0)"
+                            class="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-xl font-light">
                         0
                     </button>
                 </div>
                 <div class="w-1/4 border-r border-indigo-400">
-                    <button
-                        class="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-xl font-light">
+                    <button @click="appendNumber('.')"
+                            class="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-xl font-light">
                         .
                     </button>
                 </div>
                 <div class="w-2/4 border-r border-indigo-400">
-                    <button
-                        class="w-full h-16 outline-none focus:outline-none bg-indigo-700 bg-opacity-30 hover:bg-opacity-40 text-white text-xl font-light">
+                    <button @click="compute"
+                            class="w-full h-16 outline-none focus:outline-none bg-indigo-700 bg-opacity-30 hover:bg-opacity-40 text-white text-xl font-light">
                         =
                     </button>
                 </div>
@@ -143,7 +144,7 @@
 
 <script setup>
 
-import {ref} from "vue";
+import {ref, computed} from "vue";
 
 const previousOperand = ref('')
 const currentOperand = ref('')
@@ -154,18 +155,34 @@ function clear() {
     previousOperand.value = '';
     currentOperand.value = '';
     operation.value = '';
-    result.value = 0;
 }
+
+/*
+const result = computed(() => {
+    return currentOperand.value
+})
+*/
 
 function appendNumber(number) {
     if (number === '.' && currentOperand.value.includes('.')) return
     currentOperand.value = currentOperand.value.toString() + number.toString()
 }
 
+function appendOperation(op) {
+    operation.value = op;
+    previousOperand.value = currentOperand.value;
+    currentOperand.value = '';
+}
+
 function compute() {
+
+    if (!previousOperand.value.length || !currentOperand.value.length) {
+        return;
+    }
+
     switch (operation.value) {
         case '+':
-            result.value = previousOperand.value + currentOperand.value;
+            result.value = parseFloat(previousOperand.value) + parseFloat(currentOperand.value);
             break;
         case '-':
             result.value = previousOperand.value - currentOperand.value;
