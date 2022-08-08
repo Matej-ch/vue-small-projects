948d9e51fcaee5a10c1d6d1aad9fec94a28308da
Matej <matejchalachan@gmail.com>
2022-08-08 12:47:17 +0200

    ADDED icons to navbar

--- store/navbarStore.js
+++ store/navbarStore.js
@@ -1,34 +1,59 @@
 import {defineStore} from 'pinia'
+import {shallowRef} from 'vue'
+import RoundCalculate from '~icons/ic/round-calculate'
+import RoundHome from '~icons/ic/round-home'
+import BaselineBorderStyle from '~icons/ic/baseline-border-style'
+import OutlineNumbers from '~icons/ic/outline-numbers'
+import RoundQueryStats from '~icons/ic/round-query-stats'
+import SharpQuestionMark from '~icons/ic/sharp-question-mark'
+import RoundSignalWifiStatusbarConnectedNoInternet4 from '~icons/ic/round-signal-wifi-statusbar-connected-no-internet-4'
+import TwotoneGradient from '~icons/ic/twotone-gradient'
+import RoundAccountBalance from '~icons/ic/round-account-balance'
+import RoundCloud from '~icons/ic/round-cloud'
+import RoundSetMeal from '~icons/ic/round-set-meal'
+import RoundCurrencyExchange from '~icons/ic/round-currency-exchange'
+import RoundLock from '~icons/ic/round-lock'
+import RoundTextFields from '~icons/ic/round-text-fields'
+import TwotonePolymer from '~icons/ic/twotone-polymer'
+import RoundFormatColorFill from '~icons/ic/round-format-color-fill'
+import RoundLocalAtm from '~icons/ic/round-local-atm'
+
+import Planet from '~icons/ion/planet'
+import Time from '~icons/ion/time'
+import Newspaper from '~icons/ion/newspaper'
+import StopwatchOutline from '~icons/ion/stopwatch-outline'
 
 export const useNavbarStore = defineStore('navbar', {
     state: () => {
         return {
             items: [
-                {url: 'index', label: 'Home', 'icon': 'ic:round-home'},
-                {url: 'border', label: 'Border radius previewer', 'icon': 'ic:baseline-border-style'},
-                {url: 'bindec', label: 'Bin 2 Dec', 'icon': 'ic:outline-numbers'},
-                {url: 'freq', label: 'Word frequency', 'icon': 'ic:round-query-stats'},
-                {url: 'truefalse', label: 'True or False', 'icon': 'ic:sharp-question-mark'},
+                {
+                    url: 'index', label: 'Home', 'icon': shallowRef(RoundHome)
+                },
+                {url: 'border', label: 'Border radius previewer', 'icon': shallowRef(BaselineBorderStyle)},
+                {url: 'bindec', label: 'Bin 2 Dec', 'icon': shallowRef(OutlineNumbers)},
+                {url: 'freq', label: 'Word frequency', 'icon': shallowRef(RoundQueryStats)},
+                {url: 'truefalse', label: 'True or False', 'icon': shallowRef(SharpQuestionMark)},
                 {
                     url: 'github',
                     label: 'Github status',
-                    'icon': 'ic:round-signal-wifi-statusbar-connected-no-internet-4'
+                    'icon': shallowRef(RoundSignalWifiStatusbarConnectedNoInternet4)
                 },
-                {url: 'calc', label: 'Calculator', 'icon': 'ic:round-calculate'},
-                {url: 'random', label: 'Random number', 'icon': 'ic:twotone-gradient'},
-                {url: 'roman', label: 'Roman numerals', 'icon': 'ic:round-account-balance'},
-                {url: 'weather', label: 'Weather App', 'icon': 'ic:round-cloud'},
-                {url: 'meal', label: 'Random Meal Generator', 'icon': 'ic:round-set-meal'},
-                {url: 'currency', label: 'Currency Converter', 'icon': 'ic:round-currency-exchange'},
-                {url: 'password', label: 'Password Generator', 'icon': 'ic:round-lock'},
-                {url: 'markdown', label: 'Markdown Previewer', 'icon': 'ic:round-text-fields'},
-                {url: 'vigenere', label: 'Vigenere Cipher', 'icon': 'ic:twotone-polymer'},
-                {url: 'color', label: 'ColorCycle', 'icon': 'ic:round-format-color-fill'},
-                {url: 'nasa', label: 'NASA Exoplanet Query', 'icon': 'ion:planet'},
-                {url: 'timeline', label: 'GitHub Timeline', 'icon': 'ion:time'},
-                {url: 'notes', label: 'Notes App', 'icon': 'ion:newspaper'},
-                {url: 'stopwatch', label: 'Stopwatch App', 'icon': 'ion:stopwatch-outline'},
-                {url: 'dollars', label: 'Dollars To Cents', 'icon': 'ic:round-local-atm'},
+                {url: 'calc', label: 'Calculator', 'icon': shallowRef(RoundCalculate)},
+                {url: 'random', label: 'Random number', 'icon': shallowRef(TwotoneGradient)},
+                {url: 'roman', label: 'Roman numerals', 'icon': shallowRef(RoundAccountBalance)},
+                {url: 'weather', label: 'Weather App', 'icon': shallowRef(RoundCloud)},
+                {url: 'meal', label: 'Random Meal Generator', 'icon': shallowRef(RoundSetMeal)},
+                {url: 'currency', label: 'Currency Converter', 'icon': shallowRef(RoundCurrencyExchange)},
+                {url: 'password', label: 'Password Generator', 'icon': shallowRef(RoundLock)},
+                {url: 'markdown', label: 'Markdown Previewer', 'icon': shallowRef(RoundTextFields)},
+                {url: 'vigenere', label: 'Vigenere Cipher', 'icon': shallowRef(TwotonePolymer)},
+                {url: 'color', label: 'ColorCycle', 'icon': shallowRef(RoundFormatColorFill)},
+                {url: 'nasa', label: 'NASA Exoplanet Query', 'icon': shallowRef(Planet)},
+                {url: 'timeline', label: 'GitHub Timeline', 'icon': shallowRef(Time)},
+                {url: 'notes', label: 'Notes App', 'icon': shallowRef(Newspaper)},
+                {url: 'stopwatch', label: 'Stopwatch App', 'icon': shallowRef(StopwatchOutline)},
+                {url: 'dollars', label: 'Dollars To Cents', 'icon': shallowRef(RoundLocalAtm)},
             ]
         };
     }
