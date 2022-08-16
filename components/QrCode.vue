46b782df46386b90bad678e85160ceca311ff7c1
Matej <matejchalachan@gmail.com>
2022-08-16 21:01:37 +0200

    qr code sizes

--- components/QrCode.vue
+++ components/QrCode.vue
@@ -7,17 +7,33 @@
         <div class="px-4 py-5">
             <div class="flex flex-col text-white">
 
-                <div class="mb-5">
-                    <div class="flex flex-col px-2 w-full">
+                <div class="mb-5 flex flex-row items-center">
+
+                    <div class="flex flex-col text-center w-3/6 px-2">
+                        <label class="mb-1">Size</label>
+
+                        <select v-model="size">
+                            <option value="100">100x100</option>
+                            <option value="200">200x200</option>
+                            <option value="300">300x300</option>
+                            <option value="400">400x400</option>
+                            <option value="500">500x500</option>
+                            <option value="600">600x600</option>
+                            <option value="700">700x700</option>
+                        </select>
+                    </div>
+
+                    <div class="flex flex-col px-2 w-3/6">
                         <label class="mb-1">Url</label>
 
                         <input v-model="url" type="text" placeholder="Insert url for qr generation"
                                @keyup.enter="generateQr">
                     </div>
+
                 </div>
 
                 <div class="flex items-center justify-between mb-5">
-                    <div id="qrcode"></div>
+                    <div id="qrcode" ref="qrImage"></div>
                 </div>
 
                 <div class="flex items-center justify-between mb-5" v-show="qrCode">
@@ -34,13 +50,17 @@
 import {ref} from "vue";
 
 const url = ref('')
+const size = ref(200)
 const qrCode = ref('');
+const qrImage = ref(null)
 
 function generateQr() {
+    qrImage.value.innerHTML = '';
+    
     qrCode.value = new QRCode('qrcode', {
         text: url.value,
-        width: 260,
-        height: 260,
+        width: size.value,
+        height: size.value,
         colorDark: "#000000",
         colorLight: "#ffffff",
     });
@@ -48,7 +68,8 @@ function generateQr() {
 
 function clearQr() {
     if (qrCode.value) {
-        qrCode.value.clear();
+        qrCode.value = null;
+        qrImage.value.innerHTML = '';
     }
 }
 
