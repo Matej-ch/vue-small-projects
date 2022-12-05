10852e4d7152f52f94dfce5b241cb0f087698a57
Matej <matejchalachan@gmail.com>
2022-12-05 21:13:18 +0100

    added gift label

---
+++ pages/label.vue
@@ -0,0 +1,23 @@
+<template>
+    <ComponentContainer>
+        <template v-slot:header>Gift label</template>
+
+        <template v-slot:body>
+            <GiftLabel/>
+        </template>
+
+    </ComponentContainer>
+</template>
+
+<script setup>
+import GiftLabel from '../components/GiftLabel'
+import {useHead} from "nuxt/app";
+
+useHead({
+    title: `Gift label`
+})
+</script>
+
+<style scoped>
+
+</style>
