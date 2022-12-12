0086f7ae214048e8bf817e82df676beb116ebf20
Matej <matejchalachan@gmail.com>
2022-12-12 21:27:39 +0100

    xmastree lights added

---
+++ components/ChristmasLights.vue
@@ -0,0 +1,39 @@
+<template>
+    <div
+        class="rounded-full bg-yellow-100 w-2 h-2 m-2 relative z-20 shadow-md twinkle"
+        :style="{
+      top: `${offsetY}px`,
+      left: `${offsetX}px`,
+      'animation-delay': `${twinkleDelay}s`,
+    }"
+    ></div>
+</template>
+
+<script setup>
+const AMPLIFY = 15
+const offsetY = Math.random() * AMPLIFY - AMPLIFY / 2
+const offsetX = Math.random() * AMPLIFY - AMPLIFY / 2
+
+// Should be the same as the length of the animation so that
+// it's evenly distributed
+const MAX_DELAY = 2
+const twinkleDelay = Math.random() * MAX_DELAY
+</script>
+
+<style scoped>
+.twinkle {
+    animation: twinkle 2s ease-in-out infinite;
+}
+
+@keyframes twinkle {
+    0% {
+        opacity: 0;
+    }
+    50% {
+        opacity: 1;
+    }
+    100% {
+        opacity: 0;
+    }
+}
+</style>
--- components/XmasTree.vue
+++ components/XmasTree.vue
@@ -1,11 +1,18 @@
 <template>
     <div>
 
-        <XmasTree v-if="size > 1" :size="size - 1"/>
+        <XmasTree v-if="size > 1" :size="size - 1">
+            <template #lights>
+                <!-- Put two lights on each section of the tree -->
+                <ChristmasLights v-for="i in 2"/>
+            </template>
+        </XmasTree>
 
         <div class="flex flex-row justify-center">
             <div v-for="i in size"
-                 class="relative rounded-full bg-green-700 w-16 h-16 -m-2 flex justify-center items-center border border-green-800"/>
+                 class="relative rounded-full bg-green-700 w-16 h-16 -m-2 flex justify-center items-center border border-green-800">
+                <slot name="lights"></slot>
+            </div>
         </div>
     </div>
 </template>
