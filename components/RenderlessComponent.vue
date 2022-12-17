03dc709ea729a9ed2d20abbedb05d1ba59a7cba5
Matej <matejchalachan@gmail.com>
2022-12-18 00:58:30 +0100

    renderless component

---
+++ components/RenderlessComponent.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="px-4">
+        <h2>Example 1</h2>
+        <NorthPoleDistance v-slot="{distance, toggleUnit, unit}">
+            You are currently {{distance}} {{unit}} away from north pole.
+            <button class="btn btn-green" @click="toggleUnit">Toggle unit</button>
+        </NorthPoleDistance>
+
+        <h2 class="mt-16">Example 2</h2>
+        <NorthPoleDistance v-slot="{distance, toggleUnit, unit}">
+            <div class="p-10 border rounded-sm">
+                Current distance to north pole: {{distance}} {{unit}}
+                <button class="btn btn-red" @click="toggleUnit">Switch to {{unit}}</button>
+            </div>
+
+        </NorthPoleDistance>
+    </div>
+</template>
+
+<script setup>
+
+import NorthPoleDistance from "~/components/NorthPoleDistance.vue";
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/renderless.vue
@@ -0,0 +1,25 @@
+<template>
+    <ComponentContainer>
+        <template v-slot:header>{{title}}
+        </template>
+
+        <template v-slot:body>
+            <RenderlessComponent/>
+        </template>
+
+    </ComponentContainer>
+</template>
+
+<script setup>
+import RenderlessComponent from '../components/RenderlessComponent'
+import {useHead} from "nuxt/app";
+
+const title = 'Renderless component';
+useHead({
+    title: title
+})
+</script>
+
+<style scoped>
+
+</style>
